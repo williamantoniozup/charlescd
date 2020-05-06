132f143e9c12d832a65ffa1ca7dae266d90e6ad8
Lucas <lucas.sales@zup.com.br>
2020-05-06 16:24:26 -0300

    update yaml and front

--- install/single-file-install/create-charles-install.sh
+++ install/single-file-install/create-charles-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash   
-modules=( "darwin-application" "darwin-deploy" "darwin-notifications" "darwin-ui-legacy" "darwin-ui-new" "darwin-villager" "keycloak" "postgresql" "nginx")
+modules=( "darwin-application" "darwin-deploy" "darwin-notifications" "darwin-ui-legacy" "darwin-ui-new" "darwin-villager" "keycloak" "postgresql" "nginx" "redis" "darwin-circle-matcher")
 rm -rf charles.yaml
 
 for i in ${modules[@]}; do
