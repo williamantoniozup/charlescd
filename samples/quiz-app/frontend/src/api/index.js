7559cc3e9674b888ebc84ed2fa6300fb009e2635
Leandro Queiroz <leandroqueiroz92@gmail.com>
2020-06-24 12:19:54 -0300

    inject header x-circle-source
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

--- samples/quiz-app/frontend/src/api/index.js
+++ samples/quiz-app/frontend/src/api/index.js
@@ -4,7 +4,8 @@ import { getCircleId } from "../utils/cookie";
 const instance = axios.create({
   headers: {
     'Content-Type': 'application/json',
-    'x-circle-id': getCircleId()
+    'x-circle-id': getCircleId(),
+    'x-circle-source': getCircleId()
   }
 });
 
