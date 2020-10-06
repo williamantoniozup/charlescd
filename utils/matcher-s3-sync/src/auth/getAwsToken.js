ee028fa2bcee625616cca01ae520ed1b4e7c2ff4
Lucas <lucas.sales@zup.com.br>
2020-10-06 18:12:10 -0300

    change s3 credentials
    Signed-off-by: Lucas <lucas.sales@zup.com.br>

--- utils/matcher-s3-sync/src/auth/getAwsToken.js
+++ utils/matcher-s3-sync/src/auth/getAwsToken.js
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { CredentialProviderChain, config, TokenFileWebIdentityCredentials } from 'aws-sdk'
+import { CredentialProviderChain, config } from 'aws-sdk'
 
 const getAwsCredentials = (callback) => {
-  const webIdentity = new TokenFileWebIdentityCredentials()
-  const authProvider = new CredentialProviderChain([webIdentity])
+  const authProvider = new CredentialProviderChain()
 
   authProvider.resolve((err, credentials) => {
     if (err) {
       console.log('Error to get cretentials from web token', err)
     }
     config.credentials = credentials
+    config.update({ region: process.env.REGION })
     callback(credentials)
   })
 }
