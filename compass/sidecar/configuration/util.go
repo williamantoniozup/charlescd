2f36c15105fc8ce14929e1536c27e7f4503ef2d1
gabrielleitezup <42280891+gabrielleitezup@users.noreply.github.com>
2020-12-03 17:39:25 -0300

    Update util.go

--- compass/sidecar/configuration/util.go
+++ compass/sidecar/configuration/util.go
@@ -5,7 +5,6 @@ import "os"
 var initialValues = map[string]string{
 	"PLUGINS_REPO": "https://github.com/gabrielleitezup/grpc-plugin-server.git",
 	"GIT_DIR":      "./tmp/git",
-	"GIT_TOKEN":    "dd3948445f9c21d369f9a962542b140792b21807",
 }
 
 func GetConfiguration(configuration string) string {
