ae9149d1a3d870ad17f71ebabdb6c1a9c3b86810
Leandro Queiroz <leandroqueiroz92@gmail.com>
2021-05-25 18:23:24 -0300

    storybook
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

--- ui/.docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/7feee3587bac01663c47b7a910307faf.js
+++
@@ -1,9 +0,0 @@
-import Text from 'core/components/Text';
-import { Playground, Props } from 'docz';
-import * as React from 'react';
-export default {
-  Text,
-  Playground,
-  Props,
-  React
-};
--- ui/.docz/src/gatsby-theme-docz/wrapper.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import THEME from 'core/assets/themes';
-
-export default ({ children }) => (
-  <ThemeProvider theme={THEME.dark}>{children}</ThemeProvider>
-)
