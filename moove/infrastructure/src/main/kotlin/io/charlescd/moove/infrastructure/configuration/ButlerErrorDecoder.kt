f33f91468601de0335e79ae9982abb014e4061fe
thalles <thalles.freitas@zup.com.br>
2021-09-17 13:25:49 -0300

    code gardening
    Signed-off-by: thalles <thalles.freitas@zup.com.br>

--- moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/configuration/ButlerErrorDecoder.kt
+++ moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/configuration/ButlerErrorDecoder.kt
@@ -3,23 +3,17 @@ package io.charlescd.moove.infrastructure.configuration
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import feign.Response
+import feign.Util
 import feign.codec.ErrorDecoder
 import io.charlescd.moove.domain.MooveErrorCode
 import io.charlescd.moove.domain.exceptions.BusinessException
 import io.charlescd.moove.domain.exceptions.NotFoundException
 import io.charlescd.moove.domain.exceptions.UnauthorizedException
-import java.io.IOException
-import java.lang.Exception
-import java.lang.IllegalArgumentException
-import java.lang.RuntimeException
-import java.nio.charset.StandardCharsets
 import org.slf4j.LoggerFactory
-import org.springframework.util.StreamUtils
 
 class ButlerErrorDecoder : ErrorDecoder {
     private val logger = LoggerFactory.getLogger(this.javaClass)
     override fun decode(methodKey: String?, response: Response?): Exception {
-        println("Resposta:"+response?.body())
         val responseMessage: String = getMessage(response)
         println("Mensagem"+responseMessage)
         return when (response?.status()) {
@@ -37,35 +31,31 @@ class ButlerErrorDecoder : ErrorDecoder {
     }
 
     private fun extractMessageFromResponse(response: Response?): String? {
-        var responseAsString: String? = null
-        try {
-            responseAsString = response?.body()?.let {
-                StreamUtils.copyToString(it.asInputStream(), StandardCharsets.UTF_8)
-            }
-            return responseAsString?.let {
-                getResponseAsObject(it)
-            }
-        } catch (ex: IOException) {
-            logger.error(ex.message, ex)
-            return responseAsString ?: "Error reading response of request"
+        return response?.let{
+            it -> readBody(it)
         }
     }
-    private fun getResponseAsObject(message: String): String {
-        val objectResponse = jacksonObjectMapper().readValue(message, ErrorDetailed::class.java)
-        println(objectResponse)
-        println (objectResponse.errors.map {
+    private fun readBody(response: Response): String {
+        val bodyReader = response.body().asReader()
+        return bodyReader.use {
+            val responseBody: String = Util.toString(bodyReader)
+             extractErrorMessages(jacksonObjectMapper().readValue(responseBody, ErrorAggregator::class.java))
+        }
+    }
+
+    private fun extractErrorMessages(errorAggregator: ErrorAggregator): String {
+       return errorAggregator.let{
+            it.errors.map {
                 error -> StringBuilder().appendln(error.title).appendln(error.detail)
-        })
-        return objectResponse.errors.map {
-            error -> StringBuilder().appendln(error.title).appendln(error.detail)
-        }.reduce {
+            }.reduce {
                 acc, error -> acc.appendln(error)
-        }.toString()
+            }.toString()
+        }
     }
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class ErrorDetailed(
+data class ErrorAggregator(
     val errors: Array<Error>
 )
 data class Error(
